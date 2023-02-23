f63559b916fa415980f3c4e5505aeac8fc68f591
Juanmapp <juanma1694@gmail.com>
2023-02-23 17:27:47 -0300

    Agregando clase 5

---
+++ Clase5/index.js
@@ -0,0 +1,8 @@
+let titulo = document.querySelector("h1")
+titulo.innerHTML += " Soy uncontenido nuevo desde JS"
+console.log(titulo)
+
+let parrafoEspecial = document.querySelector(".parrafo-especial")
+parrafoEspecial.innerHTML = "Un texto con algo <i> en cursiva </i>"
+console.log(parrafoEspecial)
+
