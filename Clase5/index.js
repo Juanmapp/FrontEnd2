5503a0162fffd0284503e6ca270c5e41f68a01fc
Juanmapp <juanma1694@gmail.com>
2023-02-27 14:48:25 -0300

    Seguimos con la clase 5

--- Clase5/index.js
+++ Clase5/index.js
@@ -1,6 +1,19 @@
+// let confirmaCambios = confirm("Queres cambiar el color del titulo?")
+
+// if (confirmaCambios) {
+
 let titulo = document.querySelector("h1")
 titulo.innerHTML += " Soy uncontenido nuevo desde JS"
+titulo.classList.add("titulo-destacado")
+
+let confirmaEliminarClase = confirm("Queres eliminar la clase del titulo?")
+
+if(confirmaEliminarClase) { 
+    titulo.classList.remove("titulo-destacado")
+}
+
 console.log(titulo)
+
 
 let parrafoEspecial = document.querySelector(".parrafo-especial")
 parrafoEspecial.innerHTML = "Un texto con algo <i> en cursiva </i>"
